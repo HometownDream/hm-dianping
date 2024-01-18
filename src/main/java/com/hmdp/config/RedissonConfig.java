5266dea2427f8f3e9213cbf5b82c2c6ebd22a11c
zyk <778268716@qq.com>
2024-01-18 12:32:51 +0800

    finished

---
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -0,0 +1,20 @@
+package com.hmdp.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient(){
+        // 配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://124.222.8.103:6379").setPassword("aA147258369.");
+        // 创建RedissonClient对象
+        return Redisson.create(config);
+    }
+}
